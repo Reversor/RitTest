242b6b6a503fa2e9ab18bfc5f0c2ead28e63ed7e
Rost Zhiharev <revers914@gmail.com>
2018-01-21 15:04:21 +0400

    little change for performance

--- src/ru/reversor/Main.java
+++ src/ru/reversor/Main.java
@@ -2,7 +2,6 @@ package ru.reversor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -14,16 +13,14 @@ public class Main {
         final int MAX = 500_000_000;
         List<Integer> soMuchIntegers = new Random().ints(SIZE, MIN, MAX).boxed().collect(Collectors.toList());
 
-        Map<Integer, Integer> minimums;
-        Map<Integer, Integer> maximums;
+        List<Integer> minimums;
+        List<Integer> maximums;
         final List<Integer> minValues = new ArrayList<>();
+        minValues.add(soMuchIntegers.get(0));
         final List<Integer> maxValues = new ArrayList<>();
+        maxValues.add(soMuchIntegers.get(0));
+        long s = System.currentTimeMillis();
         soMuchIntegers.forEach(el -> {
-            if (minValues.size() < 5) {
-                minValues.add(el);
-                maxValues.add(el);
-                return;
-            }
             for (int i = 0; i < minValues.size(); i++) {
                 if (el < minValues.get(i)) {
                     minValues.add(i, el);
@@ -44,10 +41,11 @@ public class Main {
 
             }
         });
-        minimums = minValues.stream().collect(Collectors.toMap(soMuchIntegers::indexOf, el -> el));
-        maximums = maxValues.stream().collect(Collectors.toMap(soMuchIntegers::indexOf, el -> el));
-        System.out.printf("5 наименьших: %s\n", minimums);
-        System.out.printf("5 наибольших: %s\n", maximums);
+        System.out.println(System.currentTimeMillis() - s);
+        minimums = minValues.stream().map(soMuchIntegers::indexOf).collect(Collectors.toList());
+        maximums = maxValues.stream().map(soMuchIntegers::indexOf).collect(Collectors.toList());
+        System.out.printf("Позиции 5 наименьших: %s\n", minimums);
+        System.out.printf("Позиции 5 наибольших: %s\n", maximums);
     }
 
 }
